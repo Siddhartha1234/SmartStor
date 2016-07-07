35d4de6d57980a363cfe4601acbb21ec576dbaa5
Siddhartha1234 <es15btech11018@iith.ac.in>
2016-07-07 17:40:18 +0530

    File Explorer functionalities started

--- app/src/main/java/com/example/hp/smartstor/CloudFileExplorer/ExplorerView.java
+++ app/src/main/java/com/example/hp/smartstor/CloudFileExplorer/ExplorerView.java
@@ -9,6 +9,7 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,10 +20,23 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.hp.smartstor.BaseActivity;
+import com.example.hp.smartstor.CloudMusicManager.ListItem;
 import com.example.hp.smartstor.R;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ExplorerView extends BaseActivity{
-
+    ArrayList<String> Fnames,Fextensions,FDateCreatedList,FSizeList =new ArrayList<>();
+    ArrayList<Boolean> isdirectoryList =new ArrayList<>();
+    clientFileExplorer client =new clientFileExplorer(rooturl);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +78,75 @@ public class ExplorerView extends BaseActivity{
 
         return super.onOptionsItemSelected(item);
     }
+    public void getDataFromServer(String foldername){
+        RequestParams params =new RequestParams();
+        params.put("foldername",foldername);
+        client.get("/",params, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.i("ExplorerView","Failed to Parse JSON");
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                try {
+                    JSONArray response = new JSONArray(responseString);
+                    Log.i("size", String.valueOf(response.length()));
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        {
+                            String fname = (getFilenamefrompath(jsonObject.getString("path")));
+                            if (fname != "err") {
+                                /*TODO add fname to Listitem's new object here */
+                                Fnames.add(fname);
+                            }
+                            Fextensions.add(jsonObject.getString("Ext"));
+                            /*TODO add extension to Listitem's new object here */
+                            FSizeList.add(jsonObject.getString("size"));
+                            /*TODO add size to Listitem's new object here */
+                            FDateCreatedList.add(getDateHelper(jsonObject.getString("dateCreated")));
+                            /*TODO add dateCreated to Listitem's new object here */
+                            isdirectoryList.add(jsonObject.getBoolean("isDirectory"));
+                            /*TODO add isDirectory to Listitem's new object here */
+
+                            /*TODO adapter.items.add(new object)*/
+                            if (i == response.length() - 1) {
+                                afterLoadingdone();
+                            }
+
+                        }
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+    }
+
+    public void afterLoadingdone(){
+        /*TODO add notifydatasetChanged here () as the above method is an Asynchronous(multithread) method so works on callbacks*/
+
+    }
+    public String getDateHelper(String x){
+        String[] y=x.split("T");
+        return y[0];
+    }
+    public String getFilenamefrompath(String path) {
+        StringBuffer sb=new StringBuffer();
+        if(path.length() >6 && path.contains("\\")) {
+            for (int i = path.length() - 6; path.charAt(i) != '\\'; i--) {
+                sb.append(path.charAt(i));
+            }
+            sb.reverse();
+            return sb.toString();
+        }
+        else
+            return "err";
+
+    }
+
 
     public void addFolderToPanel(String folder_name){
         final Button folder = new Button(getApplicationContext());
